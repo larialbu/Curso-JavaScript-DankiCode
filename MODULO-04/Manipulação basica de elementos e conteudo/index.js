5743c83a74fcc10888baa4f50b6bc8adc1f234bb
larialbu <larissaamca@gmail.com>
2023-02-10 16:03:56 -0300

    updates

--- MODULO-04/Manipulação basica de elementos e conteudo/index.js
+++ MODULO-04/Manipulação basica de elementos e conteudo/index.js
@@ -3,5 +3,5 @@ var p = document.getElementsByTagName('p')
 //p[0].innerHTML = 'manipulado via js'
 
 for (var i = 0; i < 10 ;i++){
-    p[0].innerHTML = p[0].innerHTML + i;
+    p[0].innerHTML = p[0].innerHTML + " - algo - ";
 }
